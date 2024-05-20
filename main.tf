f19609867812b0cd40e43cf57ebb286940ef058f
Abhishek15395 <98387212+Abhishek15395@users.noreply.github.com>
2024-05-20 17:32:32 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-    region = "us-east-1"  
+    region = "eu-west-1"  
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-0ac67a26390dc374d" # eu-west-1
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
